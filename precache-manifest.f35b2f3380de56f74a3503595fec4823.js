b63c2e43e7ce2730da9054be8a54bd326ebd131d
Janet <jjpeng@andrew.cmu.edu>
2020-08-13 10:28:18 -0400

    Updates

---
+++ precache-manifest.f35b2f3380de56f74a3503595fec4823.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2357d788b6ccd993c7d126aadfe2ab9d",
+    "url": "/designPortfolio/index.html"
+  },
+  {
+    "revision": "bd3eb3f0bbea947c7f84",
+    "url": "/designPortfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fcb362a65db02f7938ad",
+    "url": "/designPortfolio/static/css/main.8b47670a.chunk.css"
+  },
+  {
+    "revision": "bd3eb3f0bbea947c7f84",
+    "url": "/designPortfolio/static/js/2.387b58e8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/designPortfolio/static/js/2.387b58e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcb362a65db02f7938ad",
+    "url": "/designPortfolio/static/js/main.0757a15e.chunk.js"
+  },
+  {
+    "revision": "56be17ea584ba141f322",
+    "url": "/designPortfolio/static/js/runtime-main.c5f27390.js"
+  },
+  {
+    "revision": "96463b6e910dd5e76f7eacd9d33a40e7",
+    "url": "/designPortfolio/static/media/cover_bme.96463b6e.jpg"
+  },
+  {
+    "revision": "ed163c2aa4c0153f6c2928adecb2f78e",
+    "url": "/designPortfolio/static/media/cover_booktable.ed163c2a.jpg"
+  },
+  {
+    "revision": "851a08953285602de14eadee53aa599c",
+    "url": "/designPortfolio/static/media/cover_contentdev.851a0895.png"
+  },
+  {
+    "revision": "56ed5bd8fca82ba2e4c5d1a176b5e040",
+    "url": "/designPortfolio/static/media/cover_csa.56ed5bd8.jpg"
+  },
+  {
+    "revision": "f1d9bb0e102067c747d57b2b5980e7cc",
+    "url": "/designPortfolio/static/media/cover_csaillustration.f1d9bb0e.png"
+  },
+  {
+    "revision": "7841547431397da73e6562cedafb94b5",
+    "url": "/designPortfolio/static/media/cover_nocsos.78415474.jpg"
+  },
+  {
+    "revision": "7e6b9c637853fc0287a1752e01b38b02",
+    "url": "/designPortfolio/static/media/cover_python.7e6b9c63.png"
+  },
+  {
+    "revision": "467e508c5fa0adc74c5b1618ec0d0396",
+    "url": "/designPortfolio/static/media/cover_semantics.467e508c.jpg"
+  },
+  {
+    "revision": "0c755ec53bb169dbcba7f65aa81d5399",
+    "url": "/designPortfolio/static/media/cover_spatula.0c755ec5.jpg"
+  },
+  {
+    "revision": "47916f4f0ead305fb4348a3d97d25ba2",
+    "url": "/designPortfolio/static/media/cover_toaster.47916f4f.jpg"
+  }
+]);
